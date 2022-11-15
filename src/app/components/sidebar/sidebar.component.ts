51e20ffd87851db1a9109d99b4d0c7a722ee1b94
Isadora Gonçalves <isadora.goncalves@estudante.ufjf.br>
2022-11-15 16:20:55 -0300

    Fix: Corrige responsividade da sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  faBars = faBars;
+  shouldShowSidebar: boolean = true;
+
   categories = [
     { name: 'Lorem ipsum dolor', id: 1 },
     { name: 'Lorem ipsum dolor', id: 2 },
@@ -15,5 +19,21 @@ export class SidebarComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (window.innerWidth <= 700) {
+      this.shouldShowSidebar = false;
+    }
+  }
+
+  toggleSidebar(): void {
+    this.shouldShowSidebar = !this.shouldShowSidebar;
+  }
+
+  onResize(event: any): void {
+    if (window.innerWidth > 700) {
+      this.shouldShowSidebar = true;
+    } else {
+      this.shouldShowSidebar = false;
+    }
+  }
 }
